d24cef1cc386c226e976a8530df2c2462c79b06f
Vitali Tatarintev <Kalastiuz@gmail.com>
2024-11-28 12:58:54 +0100

    Add a health check API endpoint

--- todoapp/tests/test_main.py
+++ todoapp/tests/test_main.py
@@ -9,3 +9,9 @@ def test_read_root():
     response = client.get("/")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"message": "Hello World"}
+
+
+def test_health_check():
+    response = client.get("/health")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"status": "ok"}
