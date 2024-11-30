9fdebb88fe0436d0c9c8aeb845dcbaad8d17b283
Vitali Tatarintev <Kalastiuz@gmail.com>
2024-11-30 09:41:48 +0100

    Receive response object for /auth/register

--- todoapp/api/routers/auth.py
+++ todoapp/api/routers/auth.py
@@ -1,8 +1,22 @@
 from fastapi import APIRouter, status
+from pydantic import BaseModel
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 
+class RegisterResponse(BaseModel):
+    email: str
+    username: str
+
+
+class RegisterRequest(BaseModel):
+    email: str
+    password: str
+    password_confirmation: str
+
+
 @router.post("/register", status_code=status.HTTP_201_CREATED)
-async def register_user():
-    return {"msg": "User successfully created"}
+async def register_user(request: RegisterRequest):
+    username = request.email.split("@")[0]
+    response = RegisterResponse(email=request.email, username=username)
+    return {"msg": "User successfully created", "user": response}
--- todoapp/tests/api/routers/test_auth.py
+++ todoapp/tests/api/routers/test_auth.py
@@ -7,6 +7,16 @@ client = TestClient(app)
 
 
 def test_auth_register_successful_response():
-    response = client.post("/auth/register")
+    response = client.post(
+        "/auth/register",
+        json={
+            "email": "user@example.com",
+            "password": "pwd123",
+            "password_confirmation": "pwd123",
+        },
+    )
     assert response.status_code == status.HTTP_201_CREATED
-    assert response.json() == {"msg": "User successfully created"}
+    assert response.json() == {
+        "msg": "User successfully created",
+        "user": {"email": "user@example.com", "username": "user"},
+    }
