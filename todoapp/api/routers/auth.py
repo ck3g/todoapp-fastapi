01612bc91c04877ced967dd55badba22090d8262
Vitali Tatarintev <Kalastiuz@gmail.com>
2024-11-30 13:47:42 +0100

    Validate fields
    Validate length of the fields, and validate password and password confirmation

--- todoapp/api/routers/auth.py
+++ todoapp/api/routers/auth.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, status
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, model_validator
+from pydantic_core import PydanticCustomError
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -10,13 +11,28 @@ class RegisterResponse(BaseModel):
 
 
 class RegisterRequest(BaseModel):
-    email: str
-    password: str
-    password_confirmation: str
+    """Create user request form"""
+
+    email: str = Field(min_length=3, max_length=255)
+    password: str = Field(min_length=3, max_length=255)
+    password_confirmation: str = Field(min_length=3, max_length=255)
+
+    @model_validator(mode="after")
+    def validate_password_and_confirmation(self):
+        """Validates whether password matches password_confirmation"""
+        p = self.password
+        pc = self.password_confirmation
+        if p is not None and pc is not None and p != pc:
+            raise PydanticCustomError(
+                "confirmation_error", "Password and Password Confirmation do not match"
+            )
+
+        return self
 
 
 @router.post("/register", status_code=status.HTTP_201_CREATED)
 async def register_user(request: RegisterRequest):
+    """Creates and new user with provided credentials"""
     username = request.email.split("@")[0]
     response = RegisterResponse(email=request.email, username=username)
     return {"msg": "User successfully created", "user": response}
--- todoapp/tests/api/routers/test_auth.py
+++ todoapp/tests/api/routers/test_auth.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from fastapi import status
+import pytest
 
 from todoapp.main import app
 
@@ -20,3 +21,52 @@ def test_auth_register_successful_response():
         "msg": "User successfully created",
         "user": {"email": "user@example.com", "username": "user"},
     }
+
+
+@pytest.mark.parametrize(
+    "email, password, password_confirmation, expected_type, expected_msg",
+    [
+        (
+            "e",
+            "pass123",
+            "pass123",
+            "string_too_short",
+            "String should have at least 3 characters",
+        ),
+        (
+            "email@example.com",
+            "p",
+            "p",
+            "string_too_short",
+            "String should have at least 3 characters",
+        ),
+        (
+            "email@example.com",
+            "pass123",
+            "pass123456",
+            "confirmation_error",
+            "Password and Password Confirmation do not match",
+        ),
+    ],
+    ids=[
+        "email_string_too_short",
+        "password_string_too_short",
+        "password_and_confirmation_do_not_match",
+    ],
+)
+def test_auth_register_validation_errors(
+    email, password, password_confirmation, expected_type, expected_msg
+):
+    response = client.post(
+        "/auth/register",
+        json={
+            "email": email,
+            "password": password,
+            "password_confirmation": password_confirmation,
+        },
+    )
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    json_response = response.json()
+    for err in json_response["detail"]:
+        assert expected_type == err["type"]
+        assert expected_msg == err["msg"]
