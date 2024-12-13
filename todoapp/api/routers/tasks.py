1b0f1259a1136ce5e5a941ac6663fcfd1c1056e7
Vitali Tatarintev <Kalastiuz@gmail.com>
2024-12-13 11:29:12 +0100

    Extract tasks query methods

--- todoapp/api/routers/tasks.py
+++ todoapp/api/routers/tasks.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, HTTPException, status
-from sqlmodel import select
 
 from todoapp.api.routers.auth import UserDependency
 from todoapp.database.session import SessionDep
@@ -10,7 +9,7 @@ router = APIRouter(prefix="/tasks", tags=["tasks"])
 
 @router.get("/", status_code=status.HTTP_200_OK)
 async def read_tasks(current_user: UserDependency, session: SessionDep):
-    results = session.exec(select(Task).where(Task.user_id == current_user.id))
+    results = Task.all(session, user_id=current_user.id)
     tasks = []
     for task in results:
         tasks.append({"id": task.id, "title": task.title})
@@ -20,9 +19,7 @@ async def read_tasks(current_user: UserDependency, session: SessionDep):
 
 @router.get("/{task_id}", status_code=status.HTTP_200_OK)
 async def read_task(current_user: UserDependency, session: SessionDep, task_id: int):
-    task = session.exec(
-        select(Task).where(Task.id == task_id, Task.user_id == current_user.id)
-    ).first()
+    task = Task.find_by(session, task_id=task_id, user_id=current_user.id)
 
     if task is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
--- todoapp/models/task.py
+++ todoapp/models/task.py
@@ -1,11 +1,26 @@
 from datetime import UTC, datetime
+from typing import List
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Session, SQLModel, select
 
 
 class Task(SQLModel, table=True):
+    """Represents model to describe tasks"""
+
     id: int = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="user.id", nullable=False)
     title: str = Field(min_length=3, max_length=255, nullable=False)
     created_at: datetime = Field(default=datetime.now(UTC), nullable=False)
     updated_at: datetime = Field(default=datetime.now(UTC), nullable=False)
+
+    @classmethod
+    def find_by(cls, session: Session, task_id: int, user_id: int) -> "Task | None":
+        """Finds a task by id and user_id"""
+        return session.exec(
+            select(cls).where(cls.id == task_id, cls.user_id == user_id)
+        ).first()
+
+    @classmethod
+    def all(cls, session: Session, user_id: int) -> "List[Task]":
+        """Selects all tasks"""
+        return session.exec(select(cls).where(cls.user_id == user_id))
