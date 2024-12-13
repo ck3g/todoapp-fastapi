ac5b1079dc55e0e3958ed6afed77433a91977090
Vitali Tatarintev <Kalastiuz@gmail.com>
2024-12-13 11:31:59 +0100

    Use cls instead of self in User class methods

--- todoapp/models/user.py
+++ todoapp/models/user.py
@@ -6,6 +6,8 @@ from todoapp.security.password import hash_password
 
 
 class User(SQLModel, table=True):
+    """Represents a model to describe a user"""
+
     id: int = Field(default=None, primary_key=True)
     email: str = Field(
         index=True, min_length=3, max_length=255, unique=True, nullable=False
@@ -17,36 +19,40 @@ class User(SQLModel, table=True):
     created_at: datetime = Field(default=datetime.now(UTC), nullable=False)
 
     @classmethod
-    def find_by_email(self, session: Session, email: str) -> "User | None":
+    def find_by_email(cls, session: Session, email: str) -> "User | None":
+        """Finds a user by email"""
         return session.exec(
-            select(User).where(func.lower(User.email) == func.lower(email))
+            select(cls).where(func.lower(cls.email) == func.lower(email))
         ).first()
 
     @classmethod
-    def find_by_username(self, session: Session, username: str) -> "User | None":
+    def find_by_username(cls, session: Session, username: str) -> "User | None":
+        """Finds a user by username"""
         return session.exec(
-            select(User).where(func.lower(User.username) == func.lower(username))
+            select(cls).where(func.lower(cls.username) == func.lower(username))
         ).first()
 
     @classmethod
     def find_by_email_or_username(
-        self, session: Session, email_or_username: str
+        cls, session: Session, email_or_username: str
     ) -> "User | None":
+        """Finds a user by email or username"""
         return session.exec(
-            select(User).where(
+            select(cls).where(
                 or_(
-                    func.lower(User.email) == func.lower(email_or_username),
-                    func.lower(User.username) == func.lower(email_or_username),
+                    func.lower(cls.email) == func.lower(email_or_username),
+                    func.lower(cls.username) == func.lower(email_or_username),
                 )
             )
         ).first()
 
     @classmethod
     def create_by(
-        self, session: Session, email: str, username: str, password: str
+        cls, session: Session, email: str, username: str, password: str
     ) -> "User":
+        """Creates a new user"""
         hashed_password = hash_password(password)
-        user = User(email=email, username=username, hashed_password=hashed_password)
+        user = cls(email=email, username=username, hashed_password=hashed_password)
         session.add(user)
         session.commit()
         session.refresh(user)
