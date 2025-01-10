bf17d26d074815788ee4d690e9256a453a78319a
Vitali Tatarintev <Kalastiuz@gmail.com>
2025-01-10 10:04:34 +0100

    Organize lists tests into classes

--- todoapp/tests/api/routers/test_lists.py
+++ todoapp/tests/api/routers/test_lists.py
@@ -8,197 +8,211 @@ from sqlmodel import Session
 from todoapp.models import TaskList, User
 
 
-def test_read_lists_unauthenticated(client: TestClient):
-    response = client.get("/lists")
+class TestReadLists:
+    def test_unauthenticated(self, client: TestClient):
+        response = client.get("/lists")
 
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
-    assert response.json() == {"detail": "Not authenticated"}
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert response.json() == {"detail": "Not authenticated"}
 
+    def test_authenticated_success(
+        self, authenticated_client: Tuple[TestClient, User], create_list, create_user
+    ):
+        client, current_user = authenticated_client
 
-def test_read_lists_authenticated(
-    authenticated_client: Tuple[TestClient, User], create_list, create_user
-):
-    client, current_user = authenticated_client
+        another_user = create_user(
+            email="another-user@example.com", username="another-user"
+        )
+        list1 = create_list(user_id=current_user.id, title="List 1")
+        list2 = create_list(user_id=current_user.id, title="List 2")
+        create_list(user_id=another_user.id, title="List 3")
 
-    another_user = create_user(
-        email="another-user@example.com", username="another-user"
-    )
-    list1 = create_list(user_id=current_user.id, title="List 1")
-    list2 = create_list(user_id=current_user.id, title="List 2")
-    create_list(user_id=another_user.id, title="List 3")
+        response = client.get("/lists")
 
-    response = client.get("/lists")
-
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {
-        "lists": [
-            list1.model_dump(),
-            list2.model_dump(),
-        ]
-    }
-
-
-def test_read_single_list_unauthenticated(client: TestClient):
-    response = client.get("/lists/1")
-
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == {
+            "lists": [
+                list1.model_dump(),
+                list2.model_dump(),
+            ]
+        }
 
 
-def test_read_single_list_authenticated(
-    authenticated_client: Tuple[TestClient, User], create_list
-):
-    client, current_user = authenticated_client
+class TestReadSingleList:
+    def test_unauthenticated(self, client: TestClient):
+        response = client.get("/lists/1")
 
-    lst = create_list(user_id=current_user.id, title="List 1")
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    response = client.get(f"/lists/{lst.id}")
+    class TestAuthenticated:
+        def test_success(
+            self, authenticated_client: Tuple[TestClient, User], create_list
+        ):
+            client, current_user = authenticated_client
 
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json() == lst.model_dump()
+            lst = create_list(user_id=current_user.id, title="List 1")
 
+            response = client.get(f"/lists/{lst.id}")
 
-def test_read_single_list_authenticated_belongs_to_another_user(
-    authenticated_client: Tuple[TestClient, User], create_user, create_list
-):
-    client, _current_user = authenticated_client
+            assert response.status_code == status.HTTP_200_OK
+            assert response.json() == lst.model_dump()
 
-    user = create_user(email="user2@example.com", username="user2")
-    lst = create_list(user_id=user.id, title="List 1")
+        def test_belongs_to_another_user(
+            self,
+            authenticated_client: Tuple[TestClient, User],
+            create_user,
+            create_list,
+        ):
+            client, _current_user = authenticated_client
 
-    response = client.get(f"/lists/{lst.id}")
+            user = create_user(email="user2@example.com", username="user2")
+            lst = create_list(user_id=user.id, title="List 1")
 
-    assert response.status_code == status.HTTP_404_NOT_FOUND
-    assert response.json() == {"detail": "Not found"}
+            response = client.get(f"/lists/{lst.id}")
 
-
-def test_create_list_unauthenticated(client: TestClient):
-    response = client.post("/lists", json={"title": "New list"})
-
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
-    assert response.json() == {"detail": "Not authenticated"}
-
-
-def test_create_list_authenticated(authenticated_client: TestClient, session: Session):
-    client, current_user = authenticated_client
-    response = client.post("/lists", json={"title": "New list"})
-
-    lists = TaskList.all(session, user_id=current_user.id)
-
-    assert len(lists) > 0
-    lst = lists[0]
-    assert lst is not None
-    assert response.status_code == status.HTTP_201_CREATED
-    json_response = response.json()
-    assert json_response["id"] == lst.id
-    assert json_response["title"] == lst.title
-    assert lst.user_id == current_user.id
-    assert response.json() == lst.model_dump()
+            assert response.status_code == status.HTTP_404_NOT_FOUND
+            assert response.json() == {"detail": "Not found"}
 
 
-def test_create_list_authenticated_with_invalid_title(
-    authenticated_client: Tuple[TestClient, User], session: Session
-):
-    client, current_user = authenticated_client
-    response = client.post("/lists", json={"title": "L"})
+class TestCreateList:
+    def test_unauthenticated(self, client: TestClient):
+        response = client.post("/lists", json={"title": "New list"})
 
-    lists = TaskList.all(session, user_id=current_user.id)
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert response.json() == {"detail": "Not authenticated"}
 
-    assert len(lists) == 0
-    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
-    json_response = response.json()
-    assert json_response["detail"][0]["loc"] == ["body", "title"]
-    assert (
-        json_response["detail"][0]["msg"] == "String should have at least 3 characters"
-    )
+    class TestAuthenticated:
+        def test_success(self, authenticated_client: TestClient, session: Session):
+            client, current_user = authenticated_client
+            response = client.post("/lists", json={"title": "New list"})
 
+            lists = TaskList.all(session, user_id=current_user.id)
 
-def test_update_list_unauthenticated(client: TestClient):
-    response = client.patch("/lists/1", json={"title": "Updated title"})
+            assert len(lists) > 0
+            lst = lists[0]
+            assert lst is not None
+            assert response.status_code == status.HTTP_201_CREATED
+            json_response = response.json()
+            assert json_response["id"] == lst.id
+            assert json_response["title"] == lst.title
+            assert lst.user_id == current_user.id
+            assert response.json() == lst.model_dump()
 
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        def test_with_invalid_title(
+            self, authenticated_client: Tuple[TestClient, User], session: Session
+        ):
+            client, current_user = authenticated_client
+            response = client.post("/lists", json={"title": "L"})
 
+            lists = TaskList.all(session, user_id=current_user.id)
 
-def test_update_list_authenticated_success(
-    authenticated_client: Tuple[TestClient, User], create_list
-):
-    client, current_user = authenticated_client
-
-    lst = create_list(user_id=current_user.id, title="List title")
-
-    response = client.patch(f"/lists/{lst.id}", json={"title": "Updated title"})
-
-    assert lst.title == "Updated title"
-    assert response.status_code == status.HTTP_200_OK
-    json_response = response.json()
-    assert json_response == lst.model_dump()
-    assert json_response["title"] == "Updated title"
-
-
-def test_update_list_authenticated_blongs_to_another_user(
-    authenticated_client: Tuple[TestClient, User], create_list, create_user
-):
-    client, _current_user = authenticated_client
-
-    user = create_user(email="user2@example.com", username="user2")
-    lst = create_list(user_id=user.id, title="List title")
-
-    response = client.patch(f"/lists/{lst.id}", json={"title": "Updated title"})
-
-    assert lst.title == "List title"
-    assert response.status_code == status.HTTP_404_NOT_FOUND
-    assert response.json() == {"detail": "Not found"}
+            assert len(lists) == 0
+            assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+            json_response = response.json()
+            assert json_response["detail"][0]["loc"] == ["body", "title"]
+            assert (
+                json_response["detail"][0]["msg"]
+                == "String should have at least 3 characters"
+            )
 
 
-def test_update_list_authenticated_invalid_title(
-    authenticated_client: Tuple[TestClient, User], create_list
-):
-    client, current_user = authenticated_client
+class TestUpdateList:
+    def test_unauthenticated(self, client: TestClient):
+        response = client.patch("/lists/1", json={"title": "Updated title"})
 
-    lst = create_list(user_id=current_user.id, title="List title")
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    response = client.patch(f"/lists/{lst.id}", json={"title": "L"})
+    class TestAuthenticated:
+        def test_success(
+            self, authenticated_client: Tuple[TestClient, User], create_list
+        ):
+            client, current_user = authenticated_client
 
-    assert lst.title == "List title"
-    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
-    json_response = response.json()
-    assert json_response["detail"][0]["loc"] == ["body", "title"]
-    assert (
-        json_response["detail"][0]["msg"] == "String should have at least 3 characters"
-    )
+            lst = create_list(user_id=current_user.id, title="List title")
+
+            response = client.patch(f"/lists/{lst.id}", json={"title": "Updated title"})
+
+            assert lst.title == "Updated title"
+            assert response.status_code == status.HTTP_200_OK
+            json_response = response.json()
+            assert json_response == lst.model_dump()
+            assert json_response["title"] == "Updated title"
+
+        def test_blongs_to_another_user(
+            self,
+            authenticated_client: Tuple[TestClient, User],
+            create_list,
+            create_user,
+        ):
+            client, _current_user = authenticated_client
+
+            user = create_user(email="user2@example.com", username="user2")
+            lst = create_list(user_id=user.id, title="List title")
+
+            response = client.patch(f"/lists/{lst.id}", json={"title": "Updated title"})
+
+            assert lst.title == "List title"
+            assert response.status_code == status.HTTP_404_NOT_FOUND
+            assert response.json() == {"detail": "Not found"}
+
+        def test_invalid_title(
+            self, authenticated_client: Tuple[TestClient, User], create_list
+        ):
+            client, current_user = authenticated_client
+
+            lst = create_list(user_id=current_user.id, title="List title")
+
+            response = client.patch(f"/lists/{lst.id}", json={"title": "L"})
+
+            assert lst.title == "List title"
+            assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+            json_response = response.json()
+            assert json_response["detail"][0]["loc"] == ["body", "title"]
+            assert (
+                json_response["detail"][0]["msg"]
+                == "String should have at least 3 characters"
+            )
 
 
-def test_delete_list_unauthenticated(client: TestClient):
-    response = client.delete("/lists/1")
+class TestDeleteList:
+    def test_unauthenticated(self, client: TestClient):
+        response = client.delete("/lists/1")
 
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
+    class TestAuthenticated:
+        def test_success(
+            self,
+            authenticated_client: Tuple[TestClient, User],
+            session: Session,
+            create_list,
+        ):
+            client, current_user = authenticated_client
 
-def test_delete_list_authenticated_success(
-    authenticated_client: Tuple[TestClient, User], session: Session, create_list
-):
-    client, current_user = authenticated_client
+            lst = create_list(user_id=current_user.id, title="List title")
 
-    lst = create_list(user_id=current_user.id, title="List title")
+            response = client.delete(f"/lists/{lst.id}")
 
-    response = client.delete(f"/lists/{lst.id}")
+            assert (
+                TaskList.find_by(session, user_id=current_user.id, obj_id=lst.id)
+                is None
+            )
+            assert response.status_code == status.HTTP_204_NO_CONTENT
 
-    assert TaskList.find_by(session, user_id=current_user.id, obj_id=lst.id) is None
-    assert response.status_code == status.HTTP_204_NO_CONTENT
+        def test_belongs_to_another_user(
+            self,
+            authenticated_client: Tuple[TestClient, User],
+            session: Session,
+            create_list,
+            create_user,
+        ):
+            client, _current_user = authenticated_client
 
+            user = create_user(email="user2@example.com", username="user2")
+            lst = create_list(user_id=user.id, title="List title")
 
-def test_delete_list_authenticated_belongs_to_another_user(
-    authenticated_client: Tuple[TestClient, User],
-    session: Session,
-    create_list,
-    create_user,
-):
-    client, _current_user = authenticated_client
+            response = client.delete(f"/lists/{lst.id}")
 
-    user = create_user(email="user2@example.com", username="user2")
-    lst = create_list(user_id=user.id, title="List title")
-
-    response = client.delete(f"/lists/{lst.id}")
-
-    assert TaskList.find_by(session, user_id=user.id, obj_id=lst.id) is not None
-    assert response.status_code == status.HTTP_404_NOT_FOUND
-    assert response.json() == {"detail": "Not found"}
+            assert TaskList.find_by(session, user_id=user.id, obj_id=lst.id) is not None
+            assert response.status_code == status.HTTP_404_NOT_FOUND
+            assert response.json() == {"detail": "Not found"}
